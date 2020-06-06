500294109ac1b5592c90b809633eeeea0abf3b6c
Hitesh C. Monga <31334793+hiteshcmonga@users.noreply.github.com>
2020-06-07 00:15:10 +0530

    Delete hello.js

--- codes/VC test/hello.js
+++
@@ -1,6 +0,0 @@
-import { Resolver } from 'did-resolver';
-import { resolver as naclDidResolver } from 'nacl-did';
-const didResolver = new Resolver({ nacl: naclDidResolver });
-didResolver.resolve('did:nacl:Md8JiMIwsapml_FtQ2ngnGftNP5UmVCAUuhnLyAsPxI').then(doc => console.log);
-// You can also use ES7 async/await syntax
-const doc = await didResolver.resolve('did:nacl:Md8JiMIwsapml_FtQ2ngnGftNP5UmVCAUuhnLyAsPxI');
