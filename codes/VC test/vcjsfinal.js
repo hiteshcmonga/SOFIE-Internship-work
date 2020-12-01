8de6c2b05f7245480c536a480c876353aba9043c
Hitesh C. Monga <31334793+hiteshcmonga@users.noreply.github.com>
2020-12-02 04:35:48 +0530

    Update vcjsfinal.js

--- codes/VC test/vcjsfinal.js
+++ codes/VC test/vcjsfinal.js
@@ -36,6 +36,8 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
     }
 };
 exports.__esModule = true;
+const {performance} = require('perf_hooks');
+var t0 = performance.now()
 var fs = require("fs");
 var extendContextLoader = require('jsonld-signatures').extendContextLoader;
 var vc = require('vc-js');
@@ -48,6 +50,8 @@ var did_resolver_1 = require("did-resolver");
 var nacl_did_1 = require("nacl-did");
 var nacl_did_2 = require("nacl-did");
 var didResolver = new did_resolver_1.Resolver({ nacl: nacl_did_1.resolver });
+
+
 //custom document Loader according to the required contexts
 var documentLoader = extendContextLoader(function (url) { return __awaiter(void 0, void 0, void 0, function () {
     var controller;
@@ -127,37 +131,11 @@ function subject() {
 }
 subject();
 //the subject suite for signing VP
-function subjectsuite() {
-    return __awaiter(this, void 0, void 0, function () {
-        var sub, sdoc, _a, Ed25519KeyPair, Ed25519Signature2018, keyPair, suite;
-        return __generator(this, function (_b) {
-            switch (_b.label) {
-                case 0: return [4 /*yield*/, subject()];
-                case 1:
-                    sub = _b.sent();
-                    return [4 /*yield*/, didResolver.resolve(subjectid)];
-                case 2:
-                    sdoc = _b.sent();
-                    _a = require('jsonld-signatures'), Ed25519KeyPair = _a.Ed25519KeyPair, Ed25519Signature2018 = _a.suites.Ed25519Signature2018;
-                    return [4 /*yield*/, Ed25519KeyPair.generate()];
-                case 3:
-                    keyPair = _b.sent();
-                    keyPair.id = sub.id;
-                    keyPair.controller = sdoc;
-                    suite = new Ed25519Signature2018({
-                        verificationMethod: keyPair.id,
-                        key: keyPair
-                    });
-                    return [2 /*return*/, suite];
-            }
-        });
-    });
-}
-subjectsuite();
 //the function which creates and verifies our customised credentials and presentations
+
 function credentials() {
     return __awaiter(this, void 0, void 0, function () {
-        var sdoc, fetch, response, data, idoc, sub, issuer, iss, _a, Ed25519KeyPair, Ed25519Signature2018, keyPair, suite, issuercontroller, credential, issuer_suite, subject_suite, signedVC, vcresult, verifiableCredential, presentation, challenge, domain, vp, vpresult;
+        var sdoc, fetch, response, data, idoc, sub, issuer, iss, _a, Ed25519KeyPair, Ed25519Signature2018, keyPair, isssuite, issuercontroller, keyPairsub, sign, subsuite, credential, issuer_suite, subject_suite, signedVC, vcresult;
         return __generator(this, function (_b) {
             switch (_b.label) {
                 case 0: return [4 /*yield*/, didResolver.resolve(subjectid)
@@ -198,11 +176,23 @@ function credentials() {
                     //const idoc = await didResolver.resolve(idoc.id)
                     keyPair.id = iss.assertionMethod;
                     keyPair.controller = idoc;
-                    suite = new Ed25519Signature2018({
+                    isssuite = new Ed25519Signature2018({
                         verificationMethod: keyPair.id,
                         key: keyPair
                     });
                     issuercontroller = issuer;
+                    return [4 /*yield*/, Ed25519KeyPair.generate()];
+                case 7:
+                    keyPairsub = _b.sent();
+                    sign = keyPairsub.signer().sign;
+                    //console.log(signatureValue);
+                    keyPairsub.id = sub.id;
+                    keyPairsub.controller = sdoc;
+                    subsuite = new Ed25519Signature2018({
+                        verificationMethod: keyPairsub.id,
+                        key: keyPairsub
+                    });
+                    console.log(JSON.stringify(subsuite, null, 2));
                     credential = {
                         "@context": [
                             "https://www.w3.org/2018/credentials/v1",
@@ -212,37 +202,39 @@ function credentials() {
                         "issuanceDate": "2010-01-01T19:23:24Z",
                         "issuer": {
                             "id": idoc.id,
-                            "name": "Owner"
+                            "name": "Device"
                         },
                         "credentialSubject": {
                             "id": sdoc.id
                         }
                     };
-                    issuer_suite = suite;
-                    return [4 /*yield*/, subjectsuite()];
-                case 7:
-                    subject_suite = _b.sent();
+                    issuer_suite = isssuite;
+                    subject_suite = subsuite;
                     return [4 /*yield*/, vc.issue({ credential: credential, suite: issuer_suite })];
                 case 8:
                     signedVC = _b.sent();
                     return [4 /*yield*/, vc.verifyCredential({ credential: signedVC, documentLoader: documentLoader, suite: issuer_suite, controller: issuercontroller })];
                 case 9:
                     vcresult = _b.sent();
-                    console.log(JSON.stringify(vcresult, null, 2));
-                    verifiableCredential = [signedVC];
-                    presentation = vc.createPresentation({ verifiableCredential: verifiableCredential });
-                    challenge = "1f44d55f-f161-4938-a659-f8026467f126";
-                    domain = "4jt78h47fh47";
-                    return [4 /*yield*/, vc.signPresentation({ presentation: presentation, suite: subject_suite, challenge: challenge, domain: domain })];
-                case 10:
-                    vp = _b.sent();
-                    return [4 /*yield*/, vc.verify({ presentation: vp, documentLoader: documentLoader, challenge: challenge, domain: domain, suite: [issuer_suite, subject_suite], controller: issuercontroller })];
-                case 11:
-                    vpresult = _b.sent();
-                    console.log(JSON.stringify(vpresult, null, 2));
-                    return [2 /*return*/, signedVC];
+                    //console.log(JSON.stringify(vcresult, null, 2));
+                    if (vcresult.verified == true) {
+                        //now if the credential of ESP32 are verified by client the client will post its credentials over ESP32 webserver to get them verified
+                        console.log('DEVICE CREDENTIAL VERIFIED, now sending Client Credentials..... \n', JSON.stringify(vcresult, null, 2));
+                        var axios = require('axios');
+                        axios.post('http://192.168.43.159/', {
+                        "privateKey":"6990241810eedc6db88e4a39eafd137804a95f7b75cb4c54c807e609c3ed083e475cb21ae1b0d0bd43a597b05912fb3d7baa1ee4ebd683e0a543f748d5aa659e" //base58 to hex conversion of base58keyPair      
+                        })
+                       // axios.post('http://192.168.43.159/', {"PublicKey":"475cb21ae1b0d0bd43a597b05912fb3d7baa1ee4ebd683e0a543f748d5aa659e" })
+
+                    }
+                    else
+                        console.log('CREDENTIAL NOT VERIFIED');
+                    return [2 /*return*/];
             }
         });
     });
 }
 credentials();
+
+var t1 = performance.now()
+console.log("Call to doSomething took " + (t1 - t0) + " milliseconds.")
