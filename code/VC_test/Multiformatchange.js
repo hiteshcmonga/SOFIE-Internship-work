950b7e578c711369ecb8dbc6855f64a47fdfccad
Hitesh C. Monga <31334793+hiteshcmonga@users.noreply.github.com>
2021-09-09 18:54:11 +0300

    Delete Multiformatchange.js

--- code/VC_test/Multiformatchange.js
+++
@@ -1,8 +0,0 @@
-const multicodec = require('multicodec')
-var uint8array = new TextEncoder("utf-8").encode("HLG6lNaQebgoP73DUEVptln1SKE0uU9p");
-const prefixedProtobuf = multicodec.addPrefix('ed25519-pub',uint8array );
-
-const multibase = require('multibase')
-const bytes = multibase.encode('base58btc',prefixedProtobuf);
-var string = new TextDecoder().decode(bytes);
-console.log(string);
