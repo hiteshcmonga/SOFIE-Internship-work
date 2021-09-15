b26fd68e6d3bafa3a413bd61787ee39c84609726
Hitesh C. Monga <31334793+hiteshcmonga@users.noreply.github.com>
2021-09-15 11:40:27 +0300

    Update deviceCode.ino

--- code/deviceCode/deviceCode.ino
+++ code/deviceCode/deviceCode.ino
@@ -15,10 +15,10 @@ uint8_t privateKey[32];
 uint8_t publicKey[32];
 uint8_t message[2] = {1, 2};
 uint8_t signature[64];
-char encodedPublicKey[44];
+char encodedPublicKey[47];
 String didKey;
 
-/*
+
   //function to print HEX chars
   void printNumber(const uint8_t *x, uint8_t len)
   {
@@ -30,23 +30,23 @@ String didKey;
     }
     Serial.println();
   }
-*/
 
 
-//function for keypair generation
-void keypair() {
+
+//function for keypair generation, for testing only, after finalisation will put this in different header
+void didKey() {
   Ed25519::generatePrivateKey(privateKey);
   Ed25519::derivePublicKey(publicKey, privateKey);
-}
-
-
-//did:key generation.
-void didGeneration() {
-  keypair();
-  base58Encode(publicKey, 32, encodedPublicKey, 44);
-  didKey = "did:key:z6Mk" + String(encodedPublicKey);
-  didKey = didKey.substring(0, 56);
+  uint8_t start_byte[1]={0xed};
+  uint8_t concatpublicKey[sizeof(start_byte)+sizeof(publicKey)];
+  memcpy(concatpublicKey,start_byte, sizeof(start_byte));
+  memcpy(concatpublicKey + sizeof(start_byte), publicKey, sizeof(publicKey));
+ // printNumber(concatpublicKey,sizeof(start_byte)+sizeof(publicKey));
+  base58Encode(concatpublicKey, 32, encodedPublicKey, 47);
+  didKey = "did:key:z" + String(encodedPublicKey);
+  didKey=didKey.substring(0,52);
   Serial.println(didKey);
+
 }
 
 void clientCert() {
@@ -75,6 +75,7 @@ void setup() {
   }
 
   Serial.println(WiFi.localIP());
+  didKey();
   deviceDid();
   clientCert();
   server.begin();
