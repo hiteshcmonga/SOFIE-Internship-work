48d75f60a8eb717fc02458385d2daefe53676ecd
Hitesh C. Monga <31334793+hiteshcmonga@users.noreply.github.com>
2021-09-30 01:16:22 +0300

    Update owner.js

--- code/VC_test/owner.js
+++ code/VC_test/owner.js
@@ -1,15 +1,30 @@
-const vc = require('vc-js');
-const axios= require ('axios');
-const {extendContextLoader} = require('jsonld-signatures');
+// Set options as a parameter, environment variable, or rc file.
+// eslint-disable-next-line no-global-assign
+require = require("esm")(module/* , options */)
+module.exports = require("./main.js")
+
+//contexts required
 const v1 = require('./contexts/v1.json');
 const v1ex= require('./contexts/v1example.json');
 const odrl= require('./contexts/odrl.json');
-import {Ed25519VerificationKey2020} from
-  '@digitalbazaar/ed25519-verification-key-2020';
-import {Ed25519Signature2020} from '@digitalbazaar/ed25519-signature-2020';
+const ed25519v1= require('./contexts/ed25519-signature-2020-v1.json')
+
+//for sending and receiving data from esp32 webserver
+const axios= require ('axios');
+
+//signing 
 import jsigs from 'jsonld-signatures';
 const {purposes: {AssertionProofPurpose}} = jsigs;
 
+//for issuing and verifying vc
+import vc from '@digitalbazaar/vc';
+const {extendContextLoader} = require('jsonld-signatures');
+
+import {Ed25519VerificationKey2020} from
+  '@digitalbazaar/ed25519-verification-key-2020';
+import {Ed25519Signature2020, suiteContext} from '@digitalbazaar/ed25519-signature-2020';
+
+const didKeyDriver = require('@digitalbazaar/did-method-key').driver();
 
 //custom document Loader according to the required contexts
 const documentLoader = extendContextLoader(async url => {
@@ -35,67 +50,90 @@ const documentLoader = extendContextLoader(async url => {
          document: v1ex
        }; 
    }     
-
-
-   // this custom context is required for running the above url, this provides info about usage of content and services.
-   if(url=='https://www.w3.org/ns/odrl.jsonld') {
+   //this is required to resolve the suite context 
+   if(url=='https://w3id.org/security/suites/ed25519-2020/v1') {
        return {
          contextUrl: null,
          documentUrl: url,
          // this did key's context should resolve
          // to the latest did-context
-         document: odrl
+         document: ed25519v1
        }; 
-     } 
+   }   
 
-   //this is used for resolved the Nacl DID.
-   /*if(url.startsWith('did:self:')) {
-       const controller = await didResolver.resolve(url)
-       return {
-         contextUrl: null,
-         documentUrl: url,
-         document: controller
-       }
-     }*/
+   // this custom context is required for running the above url, this provides info about usage of content and services.
+   if(url=='https://www.w3.org/ns/odrl.jsonld') {
+    return {
+      contextUrl: null,
+      documentUrl: url,
+      // this did key's context should resolve
+      // to the latest did-context
+      document: odrl
+    }; 
+  } 
+
+   //did key resolving
+
+     if(url.startsWith('did:key') && url.includes('#')) {
+        const did = url.split('#')[0];
+        const didKey = await didKeyDriver.get({did});
+        let doc = null;
+        for(const prop in didKey) {
+          const property = didKey[prop];
+          if(Array.isArray(property)) {
+            [doc] = property.filter(p => p.id === url);
+          }
+          if(property.id === url) {
+            doc = didKey[prop];
+          }
+          if(doc) {
+            break;
+          }
+        }
+    }
    throw new Error('No custom context support for ' + url);   
 });
 
 
-async function getDevicedid(){
+async function sendDeviceVC(){
     const deviceDid = await axios ({
-        url: "http://192.168.43.159/devicedid",
+        url: "http://192.168.43.194/devicedid",
         method: "GET"});
-        const did=String(deviceDid.data);
-        //console.log(did);
-        return did;
-}
-getDevicedid();
-
-async function deviceVC(){
-    const keyPair = await Ed25519VerificationKey2020.generate();
-    const deviceDid= await getDevicedid();
-    const suite = new Ed25519Signature2020({key: keyPair, 
-        purpose: new AssertionProofPurpose(),
-        verificationMethod:"DID:METHOD HERE"});
-    console.log(suite.purpose);
+    const did=String(deviceDid.data);
+    const didDocument = await didKeyDriver.get({did})
+    const keyPair = await Ed25519VerificationKey2020.generate(); //this needs to be a static one, tried this only for testing.
+    keyPair.id=didDocument.verificationMethod
+    keyPair.controller=didDocument
+    console.log(keyPair.id)
+    const suite = new Ed25519Signature2020({key: keyPair, verificationMethod:keyPair.id})
+    
     //unsigned credential
     const credential={
         "@context": [
             "https://www.w3.org/2018/credentials/v1",
-            "https://www.w3.org/2018/credentials/examples/v1"
+            "https://www.w3.org/2018/credentials/examples/v1",
+            "https://w3id.org/security/suites/ed25519-2020/v1"
           ],
       
           "type": ["VerifiableCredential"],
-          "issuanceDate": "2010-01-01T19:23:24Z",
-          "issuer": {
-              "id": "did:self:owner",//replace this hard coded did by auto-generated did.
-              "name": "owner"
-             },
+          "issuanceDate": "2012-01-01T19:23:24Z",
+          "expirationDate": "2022-01-01T19:23:24Z",
+           "issuer":"did:key:z6MknCCLeeHBUaHu4aHSVLDCYQW9gjVJ7a63FpMvtuVMy53T", //owner's did , can be kept any did key here , as for VerificationMethod only credentialSubject.id matters here
             "credentialSubject": {
-          "id": deviceDid, //Devicedid
-          "name": "device1"
+          "id": didDocument.id, //Devicedid
           }
         };
-    const signedVC = await vc.issue({credential, suite,documentLoader});
-    console.log(JSON.stringify(signedVC, null, 2));
-} deviceVC()
+        const signedVC = await vc.issue({credential, suite, documentLoader});
+        JSON.stringify(signedVC, null, 2);
+        console.log(signedVC);
+        
+        /*const result= await vc.verifyCredential({credential: signedVC, suite, documentLoader});
+        console.log(JSON.stringify(result, null, 2));*/
+
+       /* const postDid = await axios ({
+            method: 'POST',
+            data: signedVC,
+            url:"http://192.168.43.194/devicevc",
+          });    */
+          
+} sendDeviceVC()
