21111b2e1dbaf229c7d3ff4b50fbaf0ff92eb26f
Hitesh C. Monga <31334793+hiteshcmonga@users.noreply.github.com>
2021-10-06 21:43:21 +0300

    Update owner.js

--- code/VC_test/owner.js
+++ code/VC_test/owner.js
@@ -3,15 +3,8 @@
 require = require("esm")(module/* , options */)
 module.exports = require("./main.js")
 
-// contexts required
-const v1 = require('./contexts/v1.json');
-const v1ex = require('./contexts/v1example.json');
-const odrl = require('./contexts/odrl.json');
-const ed25519v1 = require('./contexts/ed25519-signature-2020-v1.json')
-
 // for sending and receiving data from esp32 webserver
 const axios = require('axios');
-
 const fs = require('fs');
 
 // signing 
@@ -20,7 +13,6 @@ const { purposes: { AssertionProofPurpose } } = jsigs;
 
 // for issuing and verifying vc
 import vc from '@digitalbazaar/vc';
-const { extendContextLoader } = require('jsonld-signatures');
 
 import { Ed25519VerificationKey2020 } from
   '@digitalbazaar/ed25519-verification-key-2020';
@@ -28,68 +20,8 @@ import { Ed25519Signature2020, suiteContext } from '@digitalbazaar/ed25519-signa
 
 // didKey resolver
 const didKeyDriver = require('@digitalbazaar/did-method-key').driver();
-
-// custom document Loader according to the required contexts
-
-const documentLoader = extendContextLoader(async url => {
-  //console.log("Looking for " + url)  //to check the documents required.
-  /* this document contains varioud contexts and definations 
-   related to Verifiable Credentials(VC), Verifiable Presentation (VP) 
-  and information about various signature types */
-
-  if (url == 'https://www.w3.org/2018/credentials/v1') {
-    return {
-      contextUrl: null,
-      documentUrl: url,
-      /* this did key's context should resolve
-       to the latest did-context*/
-      document: v1
-    };
-  }
-
-  /* this document contains example/format of a verifiable credential(academic)
-  , user can change the parameters according to need and
-   the doc is linked with odrl file as well. */
-  if (url == 'https://www.w3.org/2018/credentials/examples/v1') {
-    return {
-      contextUrl: null,
-      documentUrl: url,
-      document: v1ex
-    };
-  }
-  // this is required to resolve the suite context 
-  if (url == 'https://w3id.org/security/suites/ed25519-2020/v1') {
-    return {
-      contextUrl: null,
-      documentUrl: url,
-      document: ed25519v1
-    };
-  }
-
-  /* this custom context is required for running the above url,
-   this provides info about usage of content and services.*/
-  if (url == 'https://www.w3.org/ns/odrl.jsonld') {
-    return {
-      contextUrl: null,
-      documentUrl: url,
-      document: odrl
-    };
-  }
-
-
-  // did key resolving
-  if (url.startsWith('did:key') || url.includes('#')) {
-    const did = url.split('#')[0];
-    const controller = await didKeyDriver.get({ did });
-    return {
-      contextUrl: null,
-      documentUrl: url,
-      document: controller
-    }
-  }
-
-  throw new Error('No custom context support for ' + url);
-});
+// custom document loader
+import {documentLoader} from './documentLoader.js';
 
 async function sendDeviceVC() {
   const Devicedid ="did:key:z6MkhaXgBZDvotDkL5257faiztiGiC2QtKLGpbnnEGta2doK"; 
