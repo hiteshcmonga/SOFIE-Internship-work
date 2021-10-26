1487524cb5cf99632dec57ec63dae4fde78d075e
Hitesh C. Monga <31334793+hiteshcmonga@users.noreply.github.com>
2021-10-26 03:35:30 +0300

    Update client.js

--- code/VC_test/client.js
+++ code/VC_test/client.js
@@ -8,16 +8,46 @@ import { Ed25519VerificationKey2020 } from
 import { Ed25519Signature2020, suiteContext } from '@digitalbazaar/ed25519-signature-2020';
 
 const vc = require('@digitalbazaar/vc');
+const fs = require('fs');
 
-import {clientVC} from './owner.js'
 import { documentLoader } from './documentLoader.js';
+const axios = require('axios');
 
-async function createPresentation(){
-  const verifiableCredential= clientVC;
-  const presentation = vc.createPresentation({verifiableCredential});
-  const challenge="12345"; // fetched from device then imported from owner?
-  const suite = new Ed25519Signature2020({ key: keyPair, verificationMethod: keyPair.id })// which keyPair will be used or do we need a new one here?
-  const vp = await vc.signPresentation({presentation, suite, challenge, documentLoader});
-  console.log(vp);
+async function verifyDeviceVC(){
+var devicevc = await axios({url: "http://192.168.43.159/senddevicevc", method: "GET"});
+devicevc = JSON.stringify(devicevc.data,null,2); 
+
+var keyPair = fs.readFileSync("./ownerKeyPair.json");
+keyPair = JSON.parse(keyPair);
+keyPair = await Ed25519VerificationKey2020.from(keyPair);
+
+const suite = new Ed25519Signature2020({ key: keyPair, verificationMethod: keyPair.id })
+
+//devicevc can't be read, check library.
+const result = await vc.verifyCredential({ credential: devicevc, suite, documentLoader });
+console.log(result);
 }
-createPresentation();
+verifyDeviceVC();
+
+
+async function createPresentation(clientVC, clientKeyPair){
+  
+  var verifiableCredential= fs.readFileSync(clientVC);
+  verifiableCredential= JSON.parse(verifiableCredential);
+  const presentation = vc.createPresentation({verifiableCredential});
+  const challenge="12345";
+  var keyPair = fs.readFileSync(clientKeyPair);
+  keyPair = JSON.parse(keyPair);
+  keyPair = await Ed25519VerificationKey2020.from(keyPair);
+  const suite = new Ed25519Signature2020({ key: keyPair, verificationMethod: keyPair.id })
+  const vp = await vc.signPresentation({presentation, suite, challenge, documentLoader});
+  return vp;
+}
+// createPresentation("./clientcert.json","./ownerKeyPair.json");
+
+// not yet tested
+async function sendPresentation(postUrl, presentation){
+  const sendVC = await axios.post(postUrl, presentation);
+}
+
+
